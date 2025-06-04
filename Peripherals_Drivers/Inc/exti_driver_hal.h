25c600cdeb0a1315864306d1800a8ec930f2c166
Tripteron <saescobarre@unal.edu.co>
2025-06-04 10:18:24 -0500

    .h drivers
    Creando cabeceras de drivers

---
+++ Peripherals_Drivers/Inc/exti_driver_hal.h
@@ -0,0 +1,13 @@
+/*
+ * exti_driver_hal.h
+ *
+ *  Created on: Jun 4, 2025
+ *      Author: santiago
+ */
+
+#ifndef EXTI_DRIVER_HAL_H_
+#define EXTI_DRIVER_HAL_H_
+
+
+
+#endif /* EXTI_DRIVER_HAL_H_ */
---
+++ Peripherals_Drivers/Inc/timer_driver_hal.h
@@ -0,0 +1,13 @@
+/*
+ * timer_driver_hal.h
+ *
+ *  Created on: Jun 4, 2025
+ *      Author: santiago
+ */
+
+#ifndef TIMER_DRIVER_HAL_H_
+#define TIMER_DRIVER_HAL_H_
+
+
+
+#endif /* TIMER_DRIVER_HAL_H_ */
